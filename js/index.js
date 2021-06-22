d7b868eb98d336b6d7c94ff03310e0a984f94667
Minaev Vlad <vminaev.exceedteam@gmail.com>
2021-06-22 18:34:59 +0300

    Added localStorage sync

--- js/index.js
+++ js/index.js
@@ -9,7 +9,49 @@ const newName = document.getElementById("home-new");
 const field = document.getElementById("home-edit");
 const fieldValidity = document.querySelector(".home-edit__validity");
 
-updValue();
+const defaultOptions = ["Home 1", "Home 2", "Home 3"];
+let options = defaultOptions.slice();
+
+const defaultIndex = 0;
+
+// localStorage.clear();
+
+if (localStorage.options === undefined) {
+  console.log("list was empty, filled");
+  setDefaults();
+  loadDefaults();
+} else {
+  console.log("list is not emgty, loaded from lS");
+
+  loadDefaults();
+}
+
+init();
+
+function setDefaults() {
+  localStorage.options = JSON.stringify(options);
+}
+
+function loadDefaults() {
+  const parsedOptions = JSON.parse(localStorage.options);
+
+
+  parsedOptions.forEach((el) => {
+    addDOMElement(el);
+  });
+}
+
+function addDOMElement(el) {
+  const tempOpt = document.createElement("option");
+  tempOpt.text = tempOpt.value = el;
+  document.querySelector("select").append(tempOpt);
+}
+
+// tempOpt.text = tempOpt.value = newName.value;
+
+// add as html options to select
+// save to LS
+// after edit/add update data in LS
 
 function valid() {
   field.classList.remove("__invalid");
@@ -29,38 +71,38 @@ function validation() {
   if (value.length) {
     valid();
 
-    list[list.selectedIndex].value = value;
-    list[list.selectedIndex].text = value;
-    newName.blur();
-    newName.value = null;
+    return true;
   } else {
     invalid();
   }
 }
 
-function updValue() {
+function init() {
   newName.value = list[list.selectedIndex].value;
 }
 
+
 listParent.addEventListener("change", () => {
-  updValue();
+  newName.value = list[list.selectedIndex].value;
 });
 
 editBtn.addEventListener("click", () => {
-  validation();
-  updValue();
+  if (validation()) {
+    list[list.selectedIndex].value = list[list.selectedIndex].text = options[list.selectedIndex] = newName.value;
+    localStorage.options = JSON.stringify(options);
+  }
 });
 
 addBtn.addEventListener("click", () => {
   const value = newName.value.trim();
 
   if (value.length) {
-    const tempOpt = document.createElement("option");
-    tempOpt.value = newName.value;
-    tempOpt.text = newName.value;
-    document.querySelector("select").append(tempOpt);
+    options.push(value);
+    localStorage.options = JSON.stringify(options);
+    addDOMElement(value);
 
-    updValue();
+    // select added option
+    list[list.length - 1].selected = true;
   } else {
     invalid();
   }
